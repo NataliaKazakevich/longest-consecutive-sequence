f7cde5a61affd70874e1fa2b5115a92849f964fb
Natalia Kazakevich <Natallia_Kazakevich@mail.ru>
2018-10-08 17:05:54 +0300

    task

--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
 module.exports = function longestConsecutiveLength(array) {
+
 array.sort(function(a, b){
   return a - b;
 });
-var first = array[0];
-var length = 1;
+
+let first = array[0];
+
+let length = 1;
 if (array.length === 0) {
   return 0;
 }
-var max = 1;
-var numElem = 0;
-for (var i = 1; i < array.length; i++) {
+
+let max = 1;
+
+let numElem = 0;
+for (let i = 1; i < array.length; i++) {
   if (first == (array[i] - 1) ) {
     length++;
   }else{
